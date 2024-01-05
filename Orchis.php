ec6d5ed04a9c187a4d1eb07b7a7d6a1aa4733760
AguilarNubiola <145224814+AguilarNubiola@users.noreply.github.com>
2024-01-05 04:11:26 +0100

    Add files via upload

---
+++ Orchis.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html>
+    <head><title>The coming comics</title>
+    <style>
+
+        h1 {  
+            border: 0px solid black;
+            -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease;
+        }
+
+        h1:hover {
+            transform: scale(1.2);
+        }
+
+        body {
+    background-image: url("https://assets-prd.ignimgs.com/2021/12/09/immortalxmen2022001-cvr-1639078011527.jpg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: 100%;
+   }
+
+   /* Add a black background color to the top navigation */
+.topnav {
+  background-color: white;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: yellow;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: blue;
+  color: black;
+}
+
+   img {
+     width: 300px;
+     height: 400px;
+	 border: 0px solid black;
+	 padding: 5px;
+   }
+   img:hover {
+      box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.5);
+   }
+   
+
+   footer {
+    margin: 0px;
+    padding: 0px;
+    text-align: center;
+    padding: 3px; 
+    background-color: black;
+    color: darkred;
+    font-family: sans-serif;
+    font-weight: bold;
+    font-size: 30px;
+    min-width: 100%;
+    max-width: 100%;
+    height: auto;
+   }
+
+   footer.hover {
+    opacity 0.8;
+   }
+
+   .thumbnail { 
+        display: inline-block;
+        -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease; 
+    
+} 
+
+.xmen {
+    display: inline-block;
+        -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease; 
+}
+
+/*change the number below to scale to the appropriate size*/ 
+    .thumbnail:hover { 
+        transform: scale(1.2);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+
+.xmen:hover {
+    transform: scale(1.2);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+
+div.scroll-container {
+    overflow: auto;
+    white-space: nowrap;
+    padding: 10px;
+   }
+
+
+</style></head>
+</body>
+
+<div class="topnav">
+<a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+<?php 
+   echo "It took 10 years for me to overcome fears";
+?>
+
+<?php echo "Butts are better than boobs, so I must pick back-end over front-end development"?>
+
+
+<h1 align="center"><img src="https://static.wikia.nocookie.net/logocomics/images/0/0e/X-Men%282021%29.png/revision/latest/scale-to-width-down/250?cb=20211016110239"></h1>
+
+<div class="scroll-container1">
+
+<a href="https://i.pinimg.com/originals/18/98/66/189866aea25fede7640505333d790a73.jpg" class="xmen">
+    <img src="https://i.pinimg.com/originals/18/98/66/189866aea25fede7640505333d790a73.jpg" alt="Storm">
+</a>
+
+<a href="https://static.wikia.nocookie.net/marveldatabase/images/e/e1/Max_Eisenhardt_%28Earth-616%29_from_House_of_X_Vol_1_1_001_%281%29.png/revision/latest?cb=20190724212749" class="xmen">
+    <img src="https://static.wikia.nocookie.net/marveldatabase/images/e/e1/Max_Eisenhardt_%28Earth-616%29_from_House_of_X_Vol_1_1_001_%281%29.png/revision/latest?cb=20190724212749" alt="Magneto">
+</a>
+
+<a href="https://static.wikia.nocookie.net/marveldatabase/images/d/d3/X-Men_Vol_6_1_New_Line-Up_Trading_Card_Variant_Textless.jpg/revision/latest?cb=20210911084138" class="xmen">
+    <img src="https://static.wikia.nocookie.net/marveldatabase/images/d/d3/X-Men_Vol_6_1_New_Line-Up_Trading_Card_Variant_Textless.jpg/revision/latest?cb=20210911084138" alt="Cyclops">
+</a>
+
+<a href="https://static.wikia.nocookie.net/marveldatabase/images/7/74/Marvel_Comics_Vol_1_1000_Artgerm_Collectibles_Exclusive_Emma_Frost_Virgin_Variant.jpg/revision/latest?cb=20211002082122" class="xmen">
+    <img src="https://static.wikia.nocookie.net/marveldatabase/images/7/74/Marvel_Comics_Vol_1_1000_Artgerm_Collectibles_Exclusive_Emma_Frost_Virgin_Variant.jpg/revision/latest?cb=20211002082122" alt="Emma Frost">
+</a>
+
+<a href="https://i.redd.it/rcbkhbhfd9s91.jpg" class="xmen">
+    <img src="https://i.redd.it/rcbkhbhfd9s91.jpg" alt="Apocalypse">
+</a>
+
+<a href="https://i.pinimg.com/1200x/5f/75/38/5f7538c971062f942ddfde0bd9845b0f.jpg" class="xmen">
+    <img src="https://i.pinimg.com/1200x/5f/75/38/5f7538c971062f942ddfde0bd9845b0f.jpg" alt="Laura Kinney">
+</a>
+
+<a href="https://static.tvtropes.org/pmwiki/pub/images/wolvie_2.png" class="xmen">
+    <img src="https://static.tvtropes.org/pmwiki/pub/images/wolvie_2.png" alt="Logan">
+</a>
+
+<a href="https://static.wikia.nocookie.net/doblaje/images/9/93/Nightcrawler.jpg/revision/latest?cb=20170417160843&path-prefix=es" class="xmen">
+    <img src="https://static.wikia.nocookie.net/doblaje/images/9/93/Nightcrawler.jpg/revision/latest?cb=20170417160843&path-prefix=es" alt="Nightcrawler">
+</a>
+
+<a href="https://nerdpharmaceuticals.ca/cdn/shop/products/329292144_849481649459690_8988648187655015835_n_grande.jpg?v=1675638362" class="xmen">
+    <img src="https://nerdpharmaceuticals.ca/cdn/shop/products/329292144_849481649459690_8988648187655015835_n_grande.jpg?v=1675638362" alt="Magik">
+</a>
+
+<a href="https://i.redd.it/haekf4itid5c1.jpg" class="xmen">
+    <img src="https://i.redd.it/haekf4itid5c1.jpg" alt="Shadowcat">
+</a>
+
+</div>
+
+<a href="https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2022/06/75960620212600421.jpg" class="thumbnail">
+   <img src="https://static.wikia.nocookie.net/marveldatabase/images/7/74/X-Men_Red_Vol_2_18.jpg/revision/latest?cb=20231207104323" alt="X-men Red finale, 13th of December 2023" >
+</a>
+
+
+<a href="https://gnashcomics.co.uk/wp-content/uploads/2023/08/MAR0623SMU1.jpg" class="thumbnail">
+    <img src="https://cdn.marvel.com/u/prod/marvel/i/mg/9/00/6509dd3cb510e/clean.jpg" alt="Uncanny Spider-Man #5, 20th of December 2023" >
+</a>
+
+<a href="https://cdn.imagecomics.com/assets/i/releases/1011122/w0-rldtr33-6_5ee2e4b752.jpg" class="thumbnail">
+    <img src="https://cdn.imagecomics.com/assets/i/releases/1011122/w0-rldtr33-6_5ee2e4b752.jpg" alt="Worldtr33 #6, 27th of December 2023">
+</a>
+
+<a href="https://cdn.marvel.com/u/prod/marvel/i/mg/8/90/651c61eee010b/clean.jpg" class="thumbnail">
+    <img src="https://cdn.marvel.com/u/prod/marvel/i/mg/8/90/651c61eee010b/clean.jpg" alt="Rise of the powers of X #1, 10th of January 2024" >
+</a>
+
+<a href="https://cdn.marvel.com/u/prod/marvel/i/mg/6/40/651c61eea6732/clean.jpg" class="thumbnail">
+    <img src="https://cdn.marvel.com/u/prod/marvel/i/mg/6/40/651c61eea6732/clean.jpg" alt="Resurrection of Magneto #1, 24th of January 2024" >
+</a>
+
+<a href="https://cdn.imagecomics.com/assets/i/releases/1013270/w0-rldtr33-7_7d4e873dd3.jpg" class="thumbnail">
+    <img src="https://cdn.imagecomics.com/assets/i/releases/1013270/w0-rldtr33-7_7d4e873dd3.jpg" alt="Worldtr33 #7, 31st of January 2024">
+</a>
+
+<a href="https://www.previewsworld.com/SiteImage/MainImage/STL301292.jpg" class="thumbnail">
+    <img src="https://www.previewsworld.com/SiteImage/MainImage/STL301292.jpg" alt="Rise of the Powers of X #2, 21st of February 2024" >
+</a>
+
+<a href="https://static.wixstatic.com/media/06dd7a_e749f724d01f44c381480519ee714a9b~mv2.jpeg/v1/fill/w_960,h_1478,al_c,q_85,usm_0.66_1.00_0.01/06dd7a_e749f724d01f44c381480519ee714a9b~mv2.jpeg" class="thumbnail" height="100" width="100">
+    <img src="https://static.wixstatic.com/media/06dd7a_e749f724d01f44c381480519ee714a9b~mv2.jpeg/v1/fill/w_960,h_1478,al_c,q_85,usm_0.66_1.00_0.01/06dd7a_e749f724d01f44c381480519ee714a9b~mv2.jpeg" alt="Resurrection of Magneto #2, 28th of February 2024" >
+</a>
+
+<a href="https://cdn.imagecomics.com/assets/i/releases/1014519/monstress-49_3c6e16c321.jpg" class="thumbnail">
+    <img src="https://cdn.imagecomics.com/assets/i/releases/1014519/monstress-49_3c6e16c321.jpg" alt="Monstress #49, 28th of February 2024" >
+</a>
+
+<a href="https://cdn.imagecomics.com/assets/i/releases/1014503/w0-rldtr33-8_531159d23d.jpg" class="thumbnail">
+    <img src="https://cdn.imagecomics.com/assets/i/releases/1014503/w0-rldtr33-8_531159d23d.jpg" alt="Worldtr33 #8, 28th of February 2024">
+</a>
+
+<a href="https://static.wikia.nocookie.net/marveldatabase/images/2/25/Resurrection_of_Magneto_Vol_1_3.jpg/revision/latest?cb=20231208204505" class="thumbnail" >
+    <img src="https://static.wikia.nocookie.net/marveldatabase/images/2/25/Resurrection_of_Magneto_Vol_1_3.jpg/revision/latest?cb=20231208204505" al="Resurrection of Magneto #3, 20th of March 2024">
+</a>
+
+<a href="https://cdn.marvel.com/content/1x/xmforev2024001_cover.jpg" class="thumbnail">
+    <img src="https://cdn.marvel.com/content/1x/xmforev2024001_cover.jpg" alt="X-men Forever #1, 20th of March 2024" >
+</a>
+
+<a href="https://s3.amazonaws.com/comicgeeks/comics/covers/large-3348802.jpg?1701707911" class="thumbnail">
+    <img src="https://s3.amazonaws.com/comicgeeks/comics/covers/large-3348802.jpg?1701707911" alt="Rise of the Powers of X #3, 27th of March 2024">
+</a>
+
+<div class="scroll-container">
+<div align="center">
+
+<a href="https://gaqm.org/uploads/cert_logos/logo_9841.png" class="thumbnail">
+    <img src="https://gaqm.org/uploads/cert_logos/logo_9841.png" alt="Lean Yellow Belt">
+</a>
+
+<a href="https://www.itcas.nl/wp-content/uploads/2020/10/itil4.png" class='thumbnail'>
+    <img src="https://www.itcas.nl/wp-content/uploads/2020/10/itil4.png" alt='ITIl4'>
+</a>
+
+<a href="https://www.signon.nl/wp-content/uploads/2020/05/Microsoft-Azure-Fundamentals-AZ-900.png" class="thumbnail">
+    <img src="https://www.signon.nl/wp-content/uploads/2020/05/Microsoft-Azure-Fundamentals-AZ-900.png" alt="ms900">
+</a>
+
+<a href="https://miro.medium.com/max/1000/1*EnlxKqKhibp-p9fEzvfrlA.png" class="thumbnail">
+    <img src="https://miro.medium.com/max/1000/1*EnlxKqKhibp-p9fEzvfrlA.png" alt="AZ900">
+</a>
+
+<a href="https://masteritprod.blob.core.windows.net/productimages/C_Microsoft_Certified:_Azure_Developer_Associate7a546a37-d9c9-eb11-bacc-000d3a21e9d7.png?sv=2018-03-28&sr=b&sig=XuS1zXW9c1TaBFFrcSR2DTtphR4gocBCQElxDow5nA0%3D&st=2021-10-15T09%3A34%3A51Z&se=2031-10-15T10%3A04%3A51Z&sp=r" class="thumbnail">
+    <img src="https://masteritprod.blob.core.windows.net/productimages/C_Microsoft_Certified:_Azure_Developer_Associate7a546a37-d9c9-eb11-bacc-000d3a21e9d7.png?sv=2018-03-28&sr=b&sig=XuS1zXW9c1TaBFFrcSR2DTtphR4gocBCQElxDow5nA0%3D&st=2021-10-15T09%3A34%3A51Z&se=2031-10-15T10%3A04%3A51Z&sp=r" alt="azure developer">
+</a>
+</div>
+</div>
+
+<footer>
+    <pt>Destroy all enemies</br>
+    <pt>Make more mutants</br>
+    <pt>Respect the sacred land of Krakoa</br>
+    <pt>Defend Arakko</br>
+
+</footer>
+
+</body>
+
+
+
+</html>
